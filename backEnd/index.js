1748e79e76605ffc163d73ea6bdd2bebf39055a7
Om Nigam <om2021064@akgec.ac.in>
2023-10-14 16:52:07 +0530

    adding base files

---
+++ backEnd/index.js
@@ -0,0 +1,100 @@
+const express = require("express");
+const app = express();
+const fs = require("fs");
+const path = require("path");
+const PORT = 3000;
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const pathDir = path.join(__dirname, "../database/storage.json");
+
+//get All the todos
+app.get("/todos", (req, res) => {
+  fs.readFile(pathDir, "utf8", (err, data) => {
+    if (err) throw err;
+    res.send(JSON.parse(data));
+  });
+});
+
+//get Single todos
+app.get("/todos/:id", (req, res) => {
+  const id = parseInt(req.params.id); //fetches id from the url itself
+  fs.readFile(pathDir, "utf8", (err, data) => {
+    if (err) throw err;
+
+    let todoList = JSON.parse(data);
+    let todo = todoList.find((item) => item.id === id);
+
+    if (todo) {
+      res.status(200).send(todo);
+    } else {
+      res.status(404).send("Todo Not Found");
+    }
+  });
+});
+
+//create todo from the body
+let id = 1;
+app.post("/todos", (req, res) => {
+  let newTodo = req.body;
+
+  fs.readFile(pathDir, "utf-8", (err, data) => {
+    if (err) throw err;
+
+    const todoList = JSON.parse(data);
+    newTodo.id = Math.floor(Math.random() * 1000);
+    todoList.push(newTodo);
+
+    fs.writeFile(pathDir, JSON.stringify(todoList), (err) => {
+      if (err) throw err;
+      res.status(200).send(todoList);
+    });
+  });
+});
+
+//update the existing Todo
+app.put("/todos/:id", (req, res) => {
+  let id = parseInt(req.params.id);
+  let upTodo = req.body;
+  fs.readFile(pathDir, "utf-8", (err, data) => {
+    if (err) throw err;
+    let todoList = JSON.parse(data);
+
+    let todoUpdate = todoList.find((todo) => todo.id === id);
+    if (todoUpdate) {
+      todoUpdate.title = upTodo.title;
+      todoUpdate.description = upTodo.description;
+      res.status(200).send(todoList);
+    } else {
+      res.status(404).send("Todo Not Found");
+    }
+  });
+});
+
+//delete the exisiting todo List
+app.delete("/todos/:id", (req, res) => {
+  let id = parseInt(req.params.id);
+  fs.readFile(pathDir, "utf-8", (err, data) => {
+    if (err) return err;
+    let todoList = JSON.parse(data);
+
+    let deleteTodo = todoList.filter((todo) => todo.id != id);
+    fs.writeFile(pathDir, JSON.stringify(deleteTodo), (err) => {
+      if (err) return err;
+
+      res.status(200).send(deleteTodo);
+    });
+  });
+});
+
+//any other garbage routes
+app.get((req, res) => {
+  res.status(404).send("No Routes Found!");
+});
+
+app.listen(PORT, () => {
+  console.log("listening on Port : " + PORT);
+});
---
+++ frontEnd/app.js
@@ -0,0 +1,45 @@
+const ul = document.querySelector(".list>ul");
+
+//get all the list and populate it over the frontend
+window.onload = function () {
+  fetch("http://localhost:3000/todos/", { method: "GET" })
+    .then((data) => data.json())
+    .then((response) => {
+      response.forEach((res) => {
+        const listItem = document.createElement("li");
+        listItem.innerHTML = `Title: ${res.title} <br><br> Description: ${res.description}`;
+
+        ul.appendChild(listItem);
+      });
+    });
+};
+
+//Post the todos to the list
+const submit = document.querySelector(".submit");
+const title = document.getElementById("titleInput");
+const description = document.getElementById("descriptionInput");
+
+submit.addEventListener("click", () => {
+  if (title.value && description.value) {
+    fetch("http://localhost:3000/todos/", {
+      method: "POST",
+      body: JSON.stringify({
+        title: title.value,
+        description: description.value,
+      }),
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((response) => {
+        if (response.of) {
+          console.log("Success!");
+        } else {
+          console.log("Failed to submit todos!");
+        }
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  } else {
+    console.log("Please Enter");
+  }
+});
